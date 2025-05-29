026b1abd384fd33750f0e98d9579052811de4062
Graeme Hill <49688915+Hillbill1987@users.noreply.github.com>
2025-05-29 16:37:19 +0100

    feat: creation of controllers
    created the football player and football team contrillers

---
+++ FootballApi/Controllers/FootballPlayerController.cs
@@ -0,0 +1,44 @@
+﻿using FootballApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballPlayerController : ControllerBase
+    {
+        static private List<FootballPlayer> players = new List<FootballPlayer>
+        {
+            new FootballPlayer
+            {
+                Id = 1,
+                FirstName = "Dave",
+                LastName = "test",
+                Age = 25,
+                Team = "Underwood"
+            },
+            new FootballPlayer
+            {
+                Id = 2,
+                FirstName = "Roger",
+                LastName= "Owens",
+                Age = 35,
+                Team = "Cola"
+            },
+            new FootballPlayer
+            {
+                Id = 3,
+                FirstName = "Kello",
+                LastName = "Dance",
+                Age =30,
+                Team = "Water"
+            }
+        };
+        [HttpGet]
+        public ActionResult<List<FootballPlayer>> GetPlayers()
+        {
+            return Ok(players);
+        }
+    }
+}
---
+++ FootballApi/Controllers/FootballTeamController.cs
@@ -0,0 +1,45 @@
+﻿using FootballApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballTeamController : ControllerBase
+    {
+        static private List<FootballTeam> Teams = new List<FootballTeam>
+        {
+            new FootballTeam
+            {
+                Id = 1,
+                Name = "Fc Cola",
+                YearFounded = 1987,
+                Country = "England",
+                Manager = "Mr T"
+            },
+            new FootballTeam
+            {
+                Id = 2,
+                Name = "RunTime Warriors",
+                YearFounded = 1912,
+                Country = "Nigeria",
+                Manager = "Mr No"
+            },
+            new FootballTeam
+            {
+                Id= 3,
+                Name = "Shavers head",
+                YearFounded = 1956,
+                Country = "Scotland",
+                Manager = "Mr Yu"
+            }
+
+        };
+        [HttpGet]
+        public ActionResult<List<FootballTeam>> GetTeams()
+        {
+            return Ok(Teams);
+        }
+    }
+}
