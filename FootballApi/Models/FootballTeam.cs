ed6d319bb998604bfa9520f71a3a8fcefd0831f8
Graeme Hill <49688915+Hillbill1987@users.noreply.github.com>
2025-05-29 15:05:39 +0100

    feat: models created
    Created models for the football player and football team

---
+++ FootballApi/Models/FootballTeam.cs
@@ -0,0 +1,11 @@
+﻿namespace FootballApi.Models
+{
+    public class FootballTeam
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int YearFounded { get; set; }
+        public string Country { get; set; }
+        public string Manager { get; set; }
+    }
+}
