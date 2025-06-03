122e0fb5a308ad27b0812bd774b50206c5a00085
Graeme Hill <49688915+Hillbill1987@users.noreply.github.com>
2025-06-03 15:47:01 +0100

    Feat creation of controllers (#3)
    * feat: creation of controllers
    
    created the football player and football team contrillers
    
    * feat: crud operations done
    
    crud operations done and tested

---
+++ FootballApi/Controllers/FootballPlayerController.cs
@@ -0,0 +1,91 @@
+﻿using FootballApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballPlayerController : ControllerBase
+    {
+        static private List<FootballPlayer> players = new List<FootballPlayer>
+        {
+            new FootballPlayer
+            {
+                Id = 1,
+                FirstName = "Dave",
+                LastName = "test",
+                Age = 25,
+                Team = "Underwood"
+            },
+            new FootballPlayer
+            {
+                Id = 2,
+                FirstName = "Roger",
+                LastName= "Owens",
+                Age = 35,
+                Team = "Cola"
+            },
+            new FootballPlayer
+            {
+                Id = 3,
+                FirstName = "Kello",
+                LastName = "Dance",
+                Age =30,
+                Team = "Water"
+            }
+        };
+        [HttpGet]
+        public ActionResult<List<FootballPlayer>> GetPlayers()
+        {
+            return Ok(players);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<FootballPlayer> GetPlayerById(int id)
+        {
+            var player = players.FirstOrDefault(x => x.Id == id);
+
+            if (player == null)
+                return NotFound();
+
+            return Ok(player);
+        }
+
+        [HttpPost]
+        public ActionResult<FootballPlayer> AddPlayer(FootballPlayer player)
+        {
+            if (player == null)
+                return BadRequest();
+            players.Add(player);
+            return CreatedAtAction(nameof(GetPlayerById), new { id = player.Id }, player);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatePlayer(int id, FootballPlayer updatedPlayer)
+        {
+            var player = players.FirstOrDefault(x => x.Id == id);
+
+            if (player == null)
+                return NotFound();
+            player.Id = updatedPlayer.Id;
+            player.FirstName = updatedPlayer.FirstName;
+            player.LastName = updatedPlayer.LastName;
+            player.Age = updatedPlayer.Age;
+            player.Team = updatedPlayer.Team;
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeletePLayer(int id)
+        {
+            var player = players.FirstOrDefault(x => x.Id == id);
+
+            if (player == null)
+                return NotFound();
+
+            players.Remove(player);
+            return NoContent();
+        }
+    }
+}
---
+++ FootballApi/Controllers/FootballTeamController.cs
@@ -0,0 +1,89 @@
+﻿using FootballApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballTeamController : ControllerBase
+    {
+        static private List<FootballTeam> Teams = new List<FootballTeam>
+        {
+            new FootballTeam
+            {
+                Id = 1,
+                Name = "Fc Cola",
+                YearFounded = 1987,
+                Country = "England",
+                Manager = "Mr T"
+            },
+            new FootballTeam
+            {
+                Id = 2,
+                Name = "RunTime Warriors",
+                YearFounded = 1912,
+                Country = "Nigeria",
+                Manager = "Mr No"
+            },
+            new FootballTeam
+            {
+                Id= 3,
+                Name = "Shavers head",
+                YearFounded = 1956,
+                Country = "Scotland",
+                Manager = "Mr Yu"
+            }
+
+        };
+        [HttpGet]
+        public ActionResult<List<FootballTeam>> GetTeams()
+        {
+            return Ok(Teams);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<FootballTeam> GetTeamById(int id)
+        {
+            var team = Teams.FirstOrDefault(t => t.Id == id);
+            if(team == null)
+                return NotFound();
+
+            return Ok(team);
+        }
+
+        [HttpPost]
+        public ActionResult AddTeam(FootballTeam team)
+        {
+            if (team == null)
+                return BadRequest();
+            Teams.Add(team);
+            return CreatedAtAction(nameof(GetTeamById), new { id = team.Id }, team);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateTeam(int id,FootballTeam updatedteam)
+        {
+            var team = Teams.FirstOrDefault(t => t.Id == id);
+            if (team == null)
+                return NotFound();
+
+            team.Id = updatedteam.Id;
+            team.Name = updatedteam.Name;
+            team.YearFounded = updatedteam.YearFounded;
+            team.Country = updatedteam.Country;
+            team.Manager = updatedteam.Manager;
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTeam(int id)
+        {
+            var team = Teams.FirstOrDefault(t => t.Id == id);
+            if (team == null)
+                return NotFound();
+
+            Teams.Remove(team);
+            return NoContent();
+        }
+
+    }
+}
